2f07939a03da6f6b31b48d2cdc80cd9d42faf47d
sajid9505 <sajid.mahmud9505@gmail.com>
2024-01-14 17:40:13 +0600

    new card added and dark theme color changed

---
+++ finance_tracker_mobile_application/lib/components/budget_card.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class BudgetCard extends StatelessWidget {
+  const BudgetCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.25,
+      margin: const EdgeInsets.all(16.0),
+      child: Card(
+        color: const Color.fromARGB(255, 2, 54, 97),
+        child: Stack(
+          children: [
+            const SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              // child: SvgPicture.asset(
+              //   'assets/BackgroundCard.svg',
+              //   fit: BoxFit.cover,
+              //   color: Colors.black,
+              // ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(16.0), // Set the desired margin
+              child: const Positioned(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 2), // Add margin at the top
+                      Text(
+                        'Food Budget',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white70,
+                        ),
+                      ),
+                      SizedBox(height: 12), // Add margin at the top
+                      Text(
+                        '2,900 TK',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white70,
+                        ),
+                      ),
+                      Text(
+                        'Left out of 10,000 TK budgeted',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white70,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(height: 20), // Add margin at the top
+                      LinearProgressIndicator(
+                        value:
+                            0.5, // Set the progress value between 0.0 and 1.0
+                        backgroundColor:
+                            Colors.grey, // Set the background color
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                            Color.fromARGB(255, 74, 241,
+                                130)), // Set the progress bar color
+                      ),
+                      SizedBox(height: 30), // Add margin at the top
+                      Text(
+                        'Calm down, you are spending too much!',
+                        style: TextStyle(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.w800,
+                          color: Colors.white70,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Add more widgets on top as needed
+          ],
+        ),
+      ),
+    );
+  }
+}
--- finance_tracker_mobile_application/lib/pages/home.dart
+++ finance_tracker_mobile_application/lib/pages/home.dart
@@ -1,4 +1,5 @@
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:finance_tracker_mobile_application/components/budget_card.dart';
+import 'package:finance_tracker_mobile_application/components/home_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,13 +8,18 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF10161e), // Set your custom background color
       appBar: AppBar(
+         backgroundColor: const Color(0xFF10161e), // Set your desired background color for the AppBar
         title: const Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               'Hello Sajid',
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white
+                ),
             ),
             Text(
               'Your finances are looking not so good...',
@@ -26,118 +32,25 @@ class HomePage extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.notifications),
+            icon: const Icon(Icons.notifications, color: Colors.white),
             onPressed: () {
               // Handle search button press
             },
           ),
           IconButton(
-            icon: const Icon(Icons.search),
+            icon: const Icon(Icons.search, color: Colors.white),
             onPressed: () {
               // Handle settings button press
             },
           ),
         ],
       ),
-      body: Container(
-        width: double.infinity,
-        height: MediaQuery.of(context).size.height * 0.5,
-        margin: const EdgeInsets.all(16.0),
-        child: Card(
-          child: Stack(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: SvgPicture.asset(
-                  'assets/BackgroundCard.svg',
-                  fit: BoxFit.cover,
-                  color: Colors.black,
-                ),
-              ),
-              Positioned(
-                // top: 16, // Adjust the top position as needed
-                // left: 55, // Adjust the left position as needed
-                child: Center(
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 16), // Add margin at the top
-                      ClipOval(
-                        child: Image.asset(
-                          'assets/Avatar.jpg',
-                          width: 100,
-                          height: 100,
-                          fit: BoxFit
-                              .cover, // Ensure the image covers the circular area
-                        ),
-                      ),
-                      const SizedBox(height: 16), // Add margin at the top
-                      const Text(
-                        'Your available balance is',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.black87,
-                        ),
-                      ),
-                      const Text(
-                        '20,983 TK',
-                        style: TextStyle(
-                          fontSize: 45,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.black87,
-                        ),
-                      ),
-                      const Text(
-                        'By this time last month you spent slightly higher (22,719 TK)',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.black87,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 16), // Add margin at the top
-                      DataTable(
-                        columns: const <DataColumn>[
-                          DataColumn(
-                            label: Expanded(
-                              child: Text(
-                                'Recent Transactions',
-                                style: TextStyle(fontStyle: FontStyle.italic),
-                              ),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Expanded(
-                              child: Text(
-                                '',
-                                style: TextStyle(fontStyle: FontStyle.italic),
-                              ),
-                            ),
-                          ),
-                        ],
-                        rows: const <DataRow>[
-                          DataRow(
-                            cells: <DataCell>[
-                              DataCell(Text('Bkash')),
-                              DataCell(Text('12,000 TK')),
-                            ],
-                          ),
-                          DataRow(
-                            cells: <DataCell>[
-                              DataCell(Text('Maa Pharmacy')),
-                              DataCell(Text('1,245 TK')),
-                            ],
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              // Add more widgets on top as needed
-            ],
-          ),
-        ),
+      body: ListView(
+        children: const [
+          HomeCard(),
+          BudgetCard()
+          // Add more widgets as needed
+        ],
       ),
     );
   }
